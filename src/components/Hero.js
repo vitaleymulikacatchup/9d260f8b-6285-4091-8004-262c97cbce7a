91651c5c1c55070aa76c3f8ed8d69c9eb6d841a3
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-30 13:54:50 +0200

    Update project files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -89,7 +89,7 @@ const Hero = () => {
           <div className="flex justify-center lg:justify-end">
             <div className="w-96 h-96 relative">
               <img 
-                src="/images/hero-3d-illustration.png" 
+                src="https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" 
                 alt="3D Abstract Illustration" 
                 className="w-full h-full object-contain"
               />
